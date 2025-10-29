916193c33b615d75d5d6a3a205b036fdb5fbca5c
a01064946264-ui <a01064946264@gmail.com>
2025-10-29 09:41:28 +0900

    [Fix] Bullet  Firing Error Fix
    총알 발사 메서드 수정

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -5,47 +5,48 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _range;
     [SerializeField] private float _damage;
-    [SerializeField] private float _deactiveTime;
-    [SerializeField] private float _shotForce;
+    [SerializeField] private int _penetration = 1;
 
-    private Rigidbody _rigidbody;
-    private float _deactCount;
+    private Vector3 _travelVector;      //총알이 이동한 거리 계산을 위한 벡터
 
-    void Awake()
-    {
-        Init();        
-    }
-
-    void OnEnable()
-    {
-        Move();
-    }
 
     void Update()
     {
-        TimeCount();   
+        Move();
+        DeactiveDistance();
     }
 
-    private void Init()
+    //총알 비활성화 메서드
+    private void DeactiveDistance()
     {
-        _rigidbody = GetComponent<Rigidbody>();
-    }
-
-    private void TimeCount()
-    {
-        _deactCount -= Time.deltaTime;
-        if (_deactCount < 0)
+        float sqrmagnitude = Vector3.SqrMagnitude(_travelVector);
+        if (sqrmagnitude >= _range * _range)
         {
             gameObject.SetActive(false);
         }
     }
 
-    public void Move()
+    //총알 관통력이 0이면 소멸하는 메서드
+    public void OnTriggerEnter(Collider other)
     {
-        _deactCount = _deactiveTime;
-        _rigidbody.AddForce(transform.forward * _shotForce, ForceMode.Impulse);
+        if (other.gameObject.CompareTag("Enemy"))
+            _penetration--;
+        if (_penetration == 0)
+            gameObject.SetActive(false);
     }
 
-   
+    public void Move()
+    {
+        if (Vector3.SqrMagnitude(_travelVector) >= _range * _range)   //_travelVector의 길이가 사정거리보다 큰 상태라면 초기화
+            _travelVector = Vector3.zero;
+
+        Vector3 direction = Time.deltaTime * _moveSpeed * Vector3.forward.normalized;
+
+        this.transform.Translate(direction , Space.Self);
+        _travelVector += direction;
+        
+
+    }
 }
--- Assets/Scripts/Manager/GlobalTime.cs
+++ Assets/Scripts/Manager/GlobalTime.cs
@@ -4,19 +4,74 @@ using UnityEngine;
 
 public class GlobalTime : MonoBehaviour
 {
-    [SerializeField] private int _dayChangeTime;
-    public int GameTime {  get; private set; }
-    public int GameWave {  get; private set; }
-    
-    
+    [SerializeField] private int _noonTime;
+    [SerializeField] private int _nightTime;
 
-    private void Awake()
+    public Day CurrentTimeZone { get; private set; }
+    public int GameTime { get; private set; }
+    public int GameWave { get; private set; }
+
+    //옵저버 패턴
+    private List<ITimeObserver> _observer = new();
+
+    public void AddObserver(ITimeObserver observer) => _observer.Add(observer);
+    public void RemoveObserver(ITimeObserver observer) => _observer.Remove(observer);
+
+    private void Notify()
+    {
+        foreach (ITimeObserver observer in _observer)
+        {
+            observer.OnNotify();
+        }
+    }
+
+
+
+    void Awake()
     {
         GameTime = 0;
         GameWave = 1;
     }
 
-    
+    void Start()
+    {
+        StartCoroutine(TimeCounting());
+        Notify();
+    }
+
+
+    private IEnumerator TimeCounting()
+    {
+        while (true)
+        {
+            GameTime++;
+            Debug.Log($"게임시간: {GameTime}");
+            TimeZoneChange();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+
+    //GameTime이 기준 시간을 넘어가면 시간대를 변경하는 메서드
+    public void TimeZoneChange()
+    {
+        switch (CurrentTimeZone)
+        {
+            case Day.Noon:
+                if (GameTime >= _noonTime)
+                    CurrentTimeZone = Day.Night;
+                break;
+
+            case Day.Night:
+                if (GameTime >= _nightTime)
+                    CurrentTimeZone = Day.Noon;
+                break;
+        }
+        GameTime = 0;                               //시간대 변경 후에는 GameTime을 초기화
+        Debug.Log($"시간대: {CurrentTimeZone}");
+
+    }
+
 }
 
 public enum Day { Noon, Night }
