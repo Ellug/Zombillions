705bddd8e789da480df7d9b325410871786c3a4f
ThePassingNoah <ansckdals319@gmail.com>
2025-10-29 12:21:44 +0900

    Squashed commit of the following:
    commit 49adc11bae6e85975ce2381765ca2d2105bc3ea5
    Merge: 45f70d5 9ca4268
    Author: Ellug <116817127+Ellug@users.noreply.github.com>
    Date:   Wed Oct 29 12:09:47 2025 +0900
    
        [Fix] Bullet, BulletSpawner for variation
    
        [Fix] Bullet, BulletSpawner for variation
    
    commit 9ca426802fe2dc1c69cbd23d4361a551dbd65313
    Author: Ellug <auron975@nate.com>
    Date:   Wed Oct 29 12:08:59 2025 +0900
    
        [Fix] Bullet, BulletSpawner for variation
    
        - Fix Bullet System
        - Apply to Player : Gunslinger Attack
    
    commit 45f70d5df8ce22a4f24fdcd3f935272fbaa041c1
    Merge: 56c4e86 49f244d
    Author: Ellug <116817127+Ellug@users.noreply.github.com>
    Date:   Wed Oct 29 09:50:50 2025 +0900
    
        [Fix] Missing Bullet Spawner in Tower Prefab
    
        [Fix] Missing Bullet Spawner in Tower Prefab
    
    commit 49f244da72f8091d2390266132907a49d40e0266
    Author: Ellug <auron975@nate.com>
    Date:   Wed Oct 29 09:50:10 2025 +0900
    
        [Fix] Missing Bullet Spawner in Tower Prefab
    
    commit 56c4e86af521d2c2fd3402a10cb34d5aa6fad448
    Merge: f2f3bf0 916193c
    Author: Ellug <116817127+Ellug@users.noreply.github.com>
    Date:   Wed Oct 29 09:42:36 2025 +0900
    
        [Merge] Bullet  Firing Error Fix
    
        [Fix] Bullet  Firing Error Fix
    
    commit f2f3bf04da87d3e9e7acdeaeff7ebf20a636272f
    Merge: f4764f8 67545d8
    Author: Ellug <116817127+Ellug@users.noreply.github.com>
    Date:   Wed Oct 29 09:42:16 2025 +0900
    
        [Merge] Add TowerSpawner
    
        [Feat] Add TowerSpawner
    
    commit 916193c33b615d75d5d6a3a205b036fdb5fbca5c
    Author: a01064946264-ui <a01064946264@gmail.com>
    Date:   Wed Oct 29 09:41:28 2025 +0900
    
        [Fix] Bullet  Firing Error Fix
    
        총알 발사 메서드 수정
    
    commit 67545d874d82acc7bd0069c342c7e3224c8313e0
    Author: KangMin9704 <wnrkdals123@gmail.com>
    Date:   Wed Oct 29 09:40:54 2025 +0900
    
        [Feat] Add TowerSpawner
    
        [Feat] TowerSpawner
         - 타워를 특정 스포너 위에서만 설치할 수 있게 처리
         - 해당 타워스포너를 클릭한 이후 QWE를 통해 해당 건물 생성
    
        [Fix] TowerData
         - 프리팹 정보도 받을 수 있게 수정
    
    commit f4764f80283b8cf674dd3e8853def02678fefae9
    Merge: 8cb7a56 5e90006
    Author: Ellug <116817127+Ellug@users.noreply.github.com>
    Date:   Wed Oct 29 09:39:03 2025 +0900
    
        [Refactor] SpwanManager, WaveManager, etc 기본 로직 수정
    
        [Refactor] SpwanManager, WaveManager, etc 기본 로직 수정

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -1,52 +1,106 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed;
-    [SerializeField] private float _range;
-    [SerializeField] private float _damage;
-    [SerializeField] private int _penetration = 1;
+    public enum BulletColor { Red, Yellow, Metal }
+    public enum BulletSize { Small, Medium, Large }
 
-    private Vector3 _travelVector;      //총알이 이동한 거리 계산을 위한 벡터
+    // 직렬화 X, 스포너에 의해서만 결정되도록 구성
+    private Vector3 _dir;
+    private float _speed;
+    private float _dmg;
+    private int _penetration;
+    private float _knockback;
+    private float _maxDistance;
+    private Vector3 _startPos;
+    private BulletColor _color;
+    private BulletSize _size;
+    private BulletSpawner _spawner;
 
+    // 파라미터 받아서 총알에 속성 부여 (방향, 속도, 대미지 등등 받아서 사용. 자체 결정X)
+    public void Init(Vector3 dir, float speed, float dmg, int pierce, float knockback, float range, BulletColor color, BulletSize size, BulletSpawner spawner)
+    {
+        _dir = dir.normalized;
+        _speed = speed;
+        _dmg = dmg;
+        _penetration = pierce;
+        _knockback = knockback;
+        _maxDistance = range;
+        _color = color;
+        _size = size;
+        _spawner = spawner;
+        _startPos = transform.position;
+
+        // 모양 변경 함수 호출
+        SetupRender();
+    }
 
     void Update()
     {
         Move();
-        DeactiveDistance();
     }
 
-    //총알 비활성화 메서드
-    private void DeactiveDistance()
-    {
-        float sqrmagnitude = Vector3.SqrMagnitude(_travelVector);
-        if (sqrmagnitude >= _range * _range)
-        {
-            gameObject.SetActive(false);
-        }
-    }
-
-    //총알 관통력이 0이면 소멸하는 메서드
+    // 총알 영역 트리거시 작동
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy"))
+        {
+            EnemyBase enemy = other.GetComponent<EnemyBase>();
+
+            // 대미지 처리
+            // enemy.TakeDamage(_dmg);
+            // 총알과 에네미 중심 사이 벡터 방향으로 넉백 부여. 넉백 메서드는 에네미에서 구현 필요
+            // enemy.Knockback((enemy.transform.position - transform.position).normalized * _knockback);
+
             _penetration--;
-        if (_penetration == 0)
-            gameObject.SetActive(false);
+
+            // 이런 경우 == 로 계산하면 오차로 인해 작동 안할 수 있어 안전하게 <=로 처리 권장
+            if (_penetration <= 0)
+                _spawner.Despawn(this);
+        }
     }
 
+    // 거리 계산 비활성화가 중복처리 돼있어 간결하게 통합.
     public void Move()
     {
-        if (Vector3.SqrMagnitude(_travelVector) >= _range * _range)   //_travelVector의 길이가 사정거리보다 큰 상태라면 초기화
-            _travelVector = Vector3.zero;
+        // 방향을 외부에서 전달받았는데 로컬 기준 방향을 사용할 이유가 없어 보여 World로 수정
+        transform.Translate(_dir * _speed * Time.deltaTime, Space.World);
 
-        Vector3 direction = Time.deltaTime * _moveSpeed * Vector3.forward.normalized;
+        // 시작점과 현재 지점 거리가 >= _maxDistance 보다 크면 비활성화로 Magnitude 계산보다 간결히 처리 가능
+        if (Vector3.Distance(_startPos, transform.position) >= _maxDistance)
+            _spawner.Despawn(this);
+    }
 
-        this.transform.Translate(direction , Space.Self);
-        _travelVector += direction;
-        
+    // 총알의 렌더러를 변경해서 프리팹 여러개 만들지 않고도 다양한 총알처럼 표현 가능
+    private void SetupRender()
+    {
+        var renderer = GetComponent<MeshRenderer>();
+        if (renderer == null) return;
 
+        switch (_color)
+        {
+            case BulletColor.Red:
+                renderer.material.color = Color.red;
+                break;
+            case BulletColor.Yellow:
+                renderer.material.color = Color.yellow;
+                break;
+            case BulletColor.Metal:
+                renderer.material.color = Color.gray;
+                break;
+        }
+
+        switch (_size)
+        {
+            case BulletSize.Small:
+                transform.localScale = Vector3.one * 0.3f;
+                break;
+            case BulletSize.Medium:
+                transform.localScale = Vector3.one * 1f;
+                break;
+            case BulletSize.Large:
+                transform.localScale = Vector3.one * 2f;
+                break;
+        }
     }
 }
