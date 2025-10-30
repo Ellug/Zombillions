02fe8219ea8ad40031c27aba1be2eced109d8fe2
ThePassingNoah <ansckdals319@gmail.com>
2025-10-30 16:20:57 +0900

    [Refactor] EnemyHpBar
    EnemyHpBar 완성
    최대 체력일 경우 HPBar 표시하지 않음
    체력이 깍일 경우 표시
    메인카메라를 바라보도록 설정

--- Assets/Scripts/Enemy/EnemyHPBar.cs
+++ Assets/Scripts/Enemy/EnemyHPBar.cs
@@ -1,17 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHPBar : MonoBehaviour
 {
-    private Transform MainCamera;
+    private Camera _MainCamera;
     private EnemyBase _enemyBase;
+    private Slider _enemyHpSlider;
+    private float MaxHP;
+
+    void Awake()
+    {
+        _enemyBase = GetComponentInParent<EnemyBase>();
+        _enemyHpSlider = GetComponent<Slider>();
+        MaxHP = _enemyBase._EnemyMAXHP;
+    }
+
     void Start()
     {
-        MainCamera = Camera.main.transform;
+        _MainCamera = Camera.main;
+        _enemyHpSlider.gameObject.SetActive(false);
+        if (_enemyHpSlider != null && _enemyBase != null)
+        {
+            _enemyHpSlider.maxValue = MaxHP;
+        }
     }
     void Update()
     {
-        transform.LookAt(transform.position + MainCamera.rotation * Vector3.forward, MainCamera.rotation * Vector3.up);
+        transform.rotation = _MainCamera.transform.rotation;
+        if (_enemyHpSlider.value < MaxHP)
+            _enemyHpSlider.gameObject.SetActive(true);
+        else
+            _enemyHpSlider.gameObject.SetActive(false);
+        _enemyHpSlider.value = _enemyBase._EnemyHP;
     }
 }
