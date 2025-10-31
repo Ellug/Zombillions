f0411d35ff712c270a167b84bb44eade0e884f38
a01064946264-ui <a01064946264@gmail.com>
2025-10-31 18:01:06 +0900

    [feat] Add Effect Sound fix Sound Manager
    Effect Sound 추가, 효과음 추가 가능
    Sound Manager와 Effect Sound 나눠서 관리 가능하게 수정

--- Assets/Scripts/Sound/EffectSoundManager.cs
+++ Assets/Scripts/Sound/EffectSoundManager.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//"EffectSoundManager" 오브젝트에 인스펙터 추가해서 사용한다.
 public class EffectSoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private AudioSource _effectSound;
+
+
     void Start()
     {
-        
+        _effectSound = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetSoundEffect(AudioClip clip)
     {
-        
+        if (clip == null)
+        {
+            Debug.LogError("효과음 사운드 트랙 추가하세요");
+            return;
+        }
+        _effectSound.PlayOneShot(clip);
     }
 }
