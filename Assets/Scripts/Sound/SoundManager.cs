89b75c1ce2f1169d7c815150173495f6615dbfc7
a01064946264-ui <a01064946264@gmail.com>
2025-11-01 19:06:44 +0900

    [feat] add BGMFadeOut
    밤낮 전환 시 BGM 교체

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 //GameManager에 인스펙터 추가해서 사용한다.
-public class SoundManager : MonoBehaviour
+public class SoundManager : MonoBehaviour , ITimeObserver
 {
     [SerializeField] private int _clipElement;
     [SerializeField] private List<AudioClip> _soundClip;
 
     public EffectSoundManager EffectSound {  get; private set; }
     private AudioSource _backGroundMugic;
-    
 
+    private GlobalTime _globalTime;
 
     void OnEnable()
     {
@@ -20,6 +20,8 @@ public class SoundManager : MonoBehaviour
     }
     void Start()
     {
+        _globalTime = GameManager.Instance.Timer;
+        _globalTime.AddObserver(this);
         _backGroundMugic = GetComponent<AudioSource>();
         GetBGMChage(_clipElement);
     }
@@ -27,6 +29,7 @@ public class SoundManager : MonoBehaviour
     {
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
+
 
     //하이라키에서 EffectSoundManager를 찾는 메서드
     private void OnSceneLoaded(Scene s, LoadSceneMode m)
@@ -49,5 +52,30 @@ public class SoundManager : MonoBehaviour
         }
         _backGroundMugic.clip = _soundClip[index];
         _backGroundMugic.Play();
+    }
+
+    //BGM체인지 때 페이드아웃
+    public void GetBGMFadeOut(int index)
+    {
+        GetBGMChage(index);
+    }
+
+    //낮밤이 바뀌면 BGM페이드아웃
+    public void OnTimeZoneChange()
+    {
+        Day timeZone = GameManager.Instance.Timer.CurrentTimeZone;
+
+        if (timeZone == Day.Noon)
+        {
+            Debug.Log("브금 낮으로 체인지");
+            GetBGMFadeOut(1);
+            return;
+        }
+        if (timeZone == Day.Night)
+        {
+            Debug.Log("브금 밤으로 체인지");
+            GetBGMFadeOut(2);
+            return;
+        }
     }
 }
