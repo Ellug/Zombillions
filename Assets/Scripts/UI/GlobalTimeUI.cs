631f8f9d91557631ea65d596f29ea0334ef2abb3
a01064946264-ui <a01064946264@gmail.com>
2025-11-02 01:13:22 +0900

    [feat] Add Find GameTime
    씬 전환시 GameTime UI 오브젝트 찾아서 UI에 시간 출력 메서드 추가

--- Assets/Scripts/UI/GlobalTimeUI.cs
+++ Assets/Scripts/UI/GlobalTimeUI.cs
@@ -1,5 +1,6 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GlobalTimeUI : MonoBehaviour
 {
@@ -8,14 +9,33 @@ public class GlobalTimeUI : MonoBehaviour
 
     private GlobalTime _globalTime;
 
+    //씬 변환 시 OnSceneLoaded 실행을 위한 델리게이트
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    //씬 전환 시 GameTime 오브젝트를 찾아 넣는 메서드
+    void OnSceneLoaded(Scene s, LoadSceneMode m)
+    {
+        if(GameObject.Find("GameTime") == null)
+            return;
+
+        GameObject gameTime = GameObject.Find("GameTime");
+        _gameTime = gameTime.GetComponent<TextMeshProUGUI>();
+    }
+
     void Start()
     {
-        _globalTime = GameManager.Instance.GetComponent<GlobalTime>();
         if (_gameTime == null)
-        {
-            // Debug.LogError("텍스트UI를 GlobarTimeUI에 넣어주세요");
             return;
-        }
+
+        _globalTime = GameManager.Instance.GetComponent<GlobalTime>();
     }
 
     void Update()
