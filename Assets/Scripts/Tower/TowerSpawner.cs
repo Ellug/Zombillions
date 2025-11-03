3fa986ffd8cf8fd1955de3ba2d7106edce0fb863
a01064946264-ui <a01064946264@gmail.com>
2025-11-03 12:30:24 +0900

    [feat] Add Tower Cost Send
    코스트 사용으로 타워 건설 기능 추가

--- Assets/Scripts/Tower/TowerSpawner.cs
+++ Assets/Scripts/Tower/TowerSpawner.cs
@@ -15,6 +15,7 @@ public class TowerSpawner : MonoBehaviour
     //public void BuildTower(TowerData.TowerTag tag, int playerGold)
     public void BuildTower(TowerData.TowerTag tag)
     {
+        GoldManager playerGold = GameManager.Instance.Gold;
         if(_isBuilt)
         {
             Debug.Log("타워가 설치되어 있습니다.");
@@ -25,12 +26,12 @@ public class TowerSpawner : MonoBehaviour
         {
             if (tower.towerTag == tag)
             {
-                //if(tower.cost > playerGold)
-                //{
-                //    Debug.Log("설치 불가 - 타워 설치에 필요한 골드가 부족합니다.");
-                //    return;
-                //}
-                //if(타워 생성 범위 안에 적이 있는가?)
+                if (tower.cost > playerGold.CurrentGold)
+                {
+                    Debug.Log("설치 불가 - 타워 설치에 필요한 골드가 부족합니다.");
+                    return;
+                }
+                //if (타워 생성 범위 안에 적이 있는가 ?)
                 //{
                 //    Debug.Log("설치 불가 - 주위에 적이 있습니다.");
                 //}
@@ -43,7 +44,9 @@ public class TowerSpawner : MonoBehaviour
                 }
                 GetComponent<BoxCollider>().enabled = false;
                 _isBuilt = true;
+
                 //플레이어 골드 cost만큼 소모처리.
+                playerGold.TrySpend(tower.cost);
                 break;
             }
         }
